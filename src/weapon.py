01b51eba8e765e8cbebdf157f958eb1840b83d61
Bentan01 <tandutran2001@gmail.com>
2022-10-26 20:40:21 -0400

    find the right sword animation

--- src/weapon.py
+++ src/weapon.py
@@ -23,17 +23,16 @@ class Sword(Weapon):
 
     def animation(self):
         self.sprites = []
-        #self.sprites.append(pygame.image.load(path.join('src','SwingSwordSplash1.png')))
-        self.sprites.append(pygame.image.load(path.join('src','SwingSplash.png')))
-        self.sprites[0] = pygame.transform.scale(self.sprites[0], (58,80))
-        #self.sprites.append(pygame.image.load(path.join('src','SwingSwordSplash2.png')))
-        #self.sprites.append(pygame.image.load(path.join('src','SwordSwing01.png')))
-        #self.sprites.append(pygame.image.load(path.join('src','SwordSwing02.png')))
-        #self.sprites.append(pygame.image.load(path.join('src','SwordSwing03.png')))
-        #self.sprites.append(pygame.image.load(path.join('src','SwordSwing04.png')))
-        #self.sprites.append(pygame.image.load(path.join('src','SwordSwing05.png')))
-        #self.sprites.append(pygame.image.load(path.join('src','SwordSwing06.png')))
-        #self.sprites.append(pygame.image.load(path.join('src','SwordSwing07.png')))
+        self.sprites.append(pygame.image.load(path.join('src','sprite_swing_sword0.png')))
+        self.sprites.append(pygame.image.load(path.join('src','sprite_swing_sword1.png')))
+        self.sprites.append(pygame.image.load(path.join('src','sprite_swing_sword2.png')))
+        self.sprites.append(pygame.image.load(path.join('src','sprite_swing_sword3.png')))
+        self.sprites.append(pygame.image.load(path.join('src','sprite_swing_sword4.png')))
+        self.sprites[0] = pygame.transform.scale(self.sprites[0], (100,100))
+        self.sprites[1] = pygame.transform.scale(self.sprites[1], (100,100))
+        self.sprites[2] = pygame.transform.scale(self.sprites[2], (100,100))
+        self.sprites[3] = pygame.transform.scale(self.sprites[3], (100,100))
+        self.sprites[4] = pygame.transform.scale(self.sprites[4], (100,100))
         self.current_sprite = 0
         self.image = self.sprites[self.current_sprite]
         self.rect = self.image.get_rect()
@@ -44,9 +43,8 @@ class Sword(Weapon):
             if self.current_sprite >= len(self.sprites)-1:
                 self.current_sprite = 0
                 self.is_animating = False
-            self.rect.topleft = [x,y]
-            surface.blit(self.image, (x, y))
-            self.current_sprite += 0.05
+            surface.blit(self.image, (x-30, y-20))
+            self.current_sprite += 0.5
             self.image = self.sprites[int(self.current_sprite)]
             print('Speed ' + str(self.current_sprite))
 
