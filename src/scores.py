42a8e33e8706fa062b8b7abd8c07db5468463af3
GowersCam <cg582888@wne.edu>
2022-11-22 13:06:16 -0500

    ft: push progression of score ft take over for Tan

--- src/scores.py
+++ src/scores.py
@@ -1,5 +1,6 @@
 import os
 import time
+import math
 
 class Score:
     def __init__(self, name):
@@ -11,6 +12,9 @@ class Score:
         self.is_no_write = True
         self.total = 0
         self.name = name
+        self.topScores = []
+        self.min_score = 0
+        self.max_score = 0
         
 
     def get_path(self):
@@ -22,7 +26,7 @@ class Score:
 
     def cal_score(self):
         if self.player_score == 0:
-            self.player_score = (self.kill_count * 100) + (self.total * .25) + 1000
+            self.player_score = math.ceil((self.kill_count * 100) + (self.total * .5) + 1000)
 
     def read_score(self):
         full_path = self.get_path()
@@ -46,6 +50,40 @@ class Score:
     def end_time(self):
         self.end_t = time.time()
         self.total += self.end_t - self.start_t
+    
+    def determine_writability(self):
+        full_path = self.get_path()
+        with open(full_path) as f:
+            lines = f.readlines()
+        lst = []
+        for i in range(len(lines)):
+            if i != 0:
+                lst.append(lines[i].split())
+        self.top_scores = lst
+        self.max_score = self.top_scores[0][1]
+        self.min_score = self.top_scores[9][1]
+        
+        if self.player_score > self.min_score:
+            if self.player_score < self.max_score:
+                i = 0
+                for i in range(1,8):
+                    if self.player_score > self.top_scores[1]:
+                        break
+                push_incrementor = len(self.top_scores) - i + 1
+                for k in range (0,push_incrementor):
+                    tmp = self.top_scores[i]
+                    if k == 0:
+                        pass
+                        
+                    
+            elif self.player_score == self.max_score:
+                other_scores = self.top_scores[1:8]
+                row = [self.name, str(self.player_score), str(self.total), str(self.kill_count),str(os.date, time.time())]
+                tmp_list = self.top_scores[0] + row + other_scores
+                self.topScores = tmp_list
+        else:
+            pass
+                
             
             
 
